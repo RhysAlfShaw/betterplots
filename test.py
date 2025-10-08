ea5ffa834234aabe39b3d4c1ab9aef7ef73e419b
Rhys Shaw <rhysc3p0@gmail.com>
2025-10-08 12:14:15 +0100

    black default and linestyle cycles

--- test.py
+++ test.py
@@ -6,8 +6,14 @@ import numpy as np
 
 bs.set_style("betterstyle")
 
-# test the cornerplot function
-np.random.seed(0)
-data = np.random.randn(10000, 5)
-fig, ax = cornerplot(data, 5)
-plt.show()
+x = np.linspace(0, 2 * np.pi, 100)
+offsets = np.linspace(0, 2 * np.pi, 15, endpoint=False)
+
+fig, ax = plt.subplots()
+for i in range(15):
+    ax.plot(x, np.sin(x - offsets[i]), label=f"Line {i+1}")
+ax.set_title("Plot with Grayscale Linestyle Cycling")
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.legend()
+plt.savefig("test.png")
